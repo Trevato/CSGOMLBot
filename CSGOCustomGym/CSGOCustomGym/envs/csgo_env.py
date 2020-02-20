174927072a4135b61b96c6224f9b7ba5bd4bb5bf
Trevor Dobbertin <trevordobbertin@gmail.com>
2020-02-19 20:11:20 -0600

    Creating the CSGO gym and creating a test training file.

--- CSGOCustomGym/CSGOCustomGym/envs/csgo_env.py
+++ CSGOCustomGym/CSGOCustomGym/envs/csgo_env.py
@@ -1,63 +1,112 @@
-import gym
-from gym import error, spaces, utils
-from gym.utils import seeding
+import cv2, gym, numpy as np
+from retro_contest.local import make
+from retro import make as make_retro
+from baselines.common.atari_wrappers import FrameStack
 
-class CSGOEnv(gym.Env):
-metadata = {'render.modes': ['human']}
+cv2.ocl.setUseOpenCL(False)
 
-    def __init__(self):
-        pass
+class PreprocessFrame(gym.ObservationWrapper):
+    """
+    Grayscaling image from three dimensional RGB pixelated images
+    - Set frame to gray
+    - Resize the frame to 96x96x1
+    """
+    def __init__(self, environment, width, height):
+        gym.ObservationWrapper.__init__(self, environment)
+        self.width = width
+        self.height = height
+        self.observation_space = gym.spaces.Box(low=0,
+                                                high=255,
+                                                shape=(self.height, self.width, 1),
+                                                dtype=np.uint8)
 
-    def _step(self, action):
+    def observation(self, image):
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        image = cv2.resize(image, (self.width, self.height), interpolation=cv2.INTER_AREA)
+        image = image[:, :, None]
+        return image
+
+
+class ActionsDiscretizer(gym.ActionWrapper):
+    """
+    Wrap a gym-retro environment and make it use discrete
+    actions for the Sonic game.
+    """
+    def __init__(self, env):
+        super(ActionsDiscretizer, self).__init__(env)
+        buttons = ["B", "A", "MODE", "START", "UP", "DOWN", "LEFT", "RIGHT", "C", "Y", "X", "Z"]
+        actions = [['LEFT'], ['RIGHT'], ['LEFT', 'DOWN'], ['RIGHT', 'DOWN'], ['DOWN'],
+                   ['DOWN', 'B'], ['B']]
+        self._actions = []
+
         """
-
-        Parameters
-        ----------
-        action :
-
-        Returns
-        -------
-        ob, reward, episode_over, info : tuple
-            ob (object) :
-                an environment-specific object representing your observation of
-                the environment.
-            reward (float) :
-                amount of reward achieved by the previous action. The scale
-                varies between environments, but the goal is always to increase
-                your total reward.
-            episode_over (bool) :
-                whether it's time to reset the environment again. Most (but not
-                all) tasks are divided up into well-defined episodes, and done
-                being True indicates the episode has terminated. (For example,
-                perhaps the pole tipped too far, or you lost your last life.)
-            info (dict) :
-                 diagnostic information useful for debugging. It can sometimes
-                 be useful for learning (for example, it might contain the raw
-                 probabilities behind the environment's last state change).
-                 However, official evaluations of your agent are not allowed to
-                 use this for learning.
+        What we do in this loop:
+        For each action in actions
+            - Create an array of 12 False (12 = nb of buttons) THESE WILL BE CHANGED!!!
+            For each button in action: (for instance ['LEFT']) we need to make that left button index = True
+                - Then the button index = LEFT = True
+            In fact at the end we will have an array where each array is an action and each elements True of this array
+            are the buttons clicked.
         """
-        self._take_action(action)
-        self.status = self.env.step()
-        reward = self._get_reward()
-        ob = self.env.getState()
-        episode_over = self.status != hfo_py.IN_GAME
-        return ob, reward, episode_over, {}
+        for action in actions:
+            _actions = np.array([False] * len(buttons))
+            for button in action:
+                _actions[buttons.index(button)] = True
+            self._actions.append(_actions)
+        self.action_space = gym.spaces.Discrete(len(self._actions))
 
-    def _reset(self):
-        pass
+    def action(self, a):
+        return self._actions[a].copy()
 
-    def _render(self, mode='human', close=False):
-        pass
+class RewardScaler(gym.RewardWrapper):
+    """
+    Bring rewards to a reasonable scale for PPO.
+    This is incredibly important and effects performance
+    drastically.
+    """
+    def reward(self, reward):
 
-    def _take_action(self, action):
-        pass
+        return reward * 0.01
 
-    def _get_reward(self):
-        """ Reward is given for XY. """
-        if self.status == FOOBAR:
-            return 1
-        elif self.status == ABC:
-            return self.somestate ** 2
-        else:
-            return 0
+def wrap_environment(environment, n_frames=4):
+    environment = ActionsDiscretizer(environment)
+    environment = RewardScaler(environment)
+    environment = PreprocessFrame(environment)
+    environment = FrameStack(environment, n_frames)
+    return environment
+
+def create_new_environment(n_frames=4):
+    """
+    Create an environment with some standard wrappers.
+
+    Trying to understand the make function.
+    """
+
+    environment = make(game="Counter-Strike Global Offensive", #Assuming just the name of the game?
+                       state=None, #Not sure what this does at the moment so giving None.
+                       bk2dir="./records")#Believe this is just a directory for records.
+
+    environment = wrap_environment(environment=environment,
+                                   n_frames=n_frames)
+
+    return environment
+
+
+def make_test_level_Green():
+    return make_test()
+
+
+def make_test(n_frames=4):
+    """
+    Create an environment with some standard wrappers.
+    """
+
+    # See comments on 'create_new_environment'
+    environment = make_retro(game="Counter-Strike Global Offensive",
+                             state=None,
+                             record="./records")
+
+    environment = wrap_environment(environment=environment,
+                                   n_frames=n_frames)
+
+    return environment
