1a995d5f744596cb7fb491a454a2fd718fd03620
Trevor Dobbertin <trevordobbertin@gmail.com>
2020-02-21 23:11:08 -0600

    Give parentheses to print statements.

--- CSGOpygame/pyscope.py
+++ CSGOpygame/pyscope.py
@@ -12,7 +12,7 @@ class pyscope :
         # http://www.karoltomala.com/blog/?p=679
         disp_no = os.getenv("DISPLAY")
         if disp_no:
-            print "I'm running under X display = {0}".format(disp_no)
+            print("I'm running under X display = {0}".format(disp_no))
 
         # Check which frame buffer drivers are available
         # Start with fbcon since directfb hangs with composite output
@@ -25,7 +25,7 @@ class pyscope :
             try:
                 pygame.display.init()
             except pygame.error:
-                print 'Driver: {0} failed.'.format(driver)
+                print('Driver: {0} failed.'.format(driver))
                 continue
             found = True
             break
@@ -34,7 +34,7 @@ class pyscope :
             raise Exception('No suitable video driver found!')
 
         size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
-        print "Framebuffer size: %d x %d" % (size[0], size[1])
+        print("Framebuffer size: %d x %d" % (size[0], size[1]))
         self.screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
         # Clear the screen to start
         self.screen.fill((0, 0, 0))
