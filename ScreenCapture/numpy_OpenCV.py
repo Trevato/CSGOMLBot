5774a819e2f3d197fc19c39e9cea5d6d913e680c
Trevor Dobbertin <trevordobbertin@gmail.com>
2020-04-19 14:42:13 -0500

    Add test method to aid in testing on Debian server.

--- ScreenCapture/numpy_OpenCV.py
+++ ScreenCapture/numpy_OpenCV.py
@@ -1,30 +1,42 @@
 import numpy as np
 import cv2
-from mss import mss
+import mss
 import time
 from PIL import Image
 
 
-with mss(display=":0.0") as sct:
-    # Part of the screen to capture
-    monitor = {"top": 40, "left": 0, "width": 800, "height": 640}
+def fast_method():
+    with mss.mss(display="0.0") as sct:
+        # Part of the screen to capture
+        monitor = {"top": 40, "left": 0, "width": 800, "height": 640}
 
-    while "Screen capturing":
-        last_time = time.time()
+        while "Screen capturing":
+            last_time = time.time()
 
-        # Get raw pixels from the screen, save it to a Numpy array
-        img = np.array(sct.grab(monitor))
+            # Get raw pixels from the screen, save it to a Numpy array
+            img = np.array(sct.grab(monitor))
 
-        # Display the picture
-        # cv2.imshow("OpenCV/Numpy normal", img)
+            # Display the picture
+            # cv2.imshow("OpenCV/Numpy normal", img)
 
-        # Display the picture in grayscale
-        # cv2.imshow('OpenCV/Numpy grayscale',
-        #            cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY))
+            # Display the picture in grayscale
+            # cv2.imshow('OpenCV/Numpy grayscale',
+            #            cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY))
 
-        print("fps: {}".format(1 / (time.time() - last_time)))
+            print("fps: {}".format(1 / (time.time() - last_time)))
 
-        # Press "q" to quit
-        # if cv2.waitKey(25) & 0xFF == ord("q"):
-        #     cv2.destroyAllWindows()
-        #     break
+            # Press "q" to quit
+            # if cv2.waitKey(25) & 0xFF == ord("q"):
+            #     cv2.destroyAllWindows()
+            #     break
+
+def test():
+    with mss.mss(display=":0.0") as sct:
+        for filename in sct.save():
+            print(filename)
+
+if __name__ == '__main__':
+    if input('Enter mode:') == 'fast':
+        fast_method()
+    elif input('Enter mode:') == 'test':
+        test()
