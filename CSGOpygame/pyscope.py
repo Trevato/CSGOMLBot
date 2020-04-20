fc40d313858ad11a2f443677cf96876b19f034b2
Trevor Dobbertin <trevordobbertin@gmail.com>
2020-04-19 19:51:10 -0500

    Tabbing off.

--- CSGOpygame/pyscope.py
+++ CSGOpygame/pyscope.py
@@ -79,29 +79,30 @@ class pyscope :
         pygame.display.update()
 
     def fast_method():
-    with mss.mss(display="0.0") as sct:
-        # Part of the screen to capture
-        monitor = {"top": 40, "left": 0, "width": 800, "height": 640}
+        with mss.mss(display="0.0") as sct:
+            # Part of the screen to capture
+            monitor = {"top": 40, "left": 0, "width": 800, "height": 640}
 
-        while "Screen capturing":
-            last_time = time.time()
+            while "Screen capturing":
+                last_time = time.time()
 
-            # Get raw pixels from the screen, save it to a Numpy array
-            img = np.array(sct.grab(monitor))
+                # Get raw pixels from the screen, save it to a Numpy array
+                img = np.array(sct.grab(monitor))
 
-            # Display the picture
-            # cv2.imshow("OpenCV/Numpy normal", img)
+                # Display the picture
+                # cv2.imshow("OpenCV/Numpy normal", img)
 
-            # Display the picture in grayscale
-            # cv2.imshow('OpenCV/Numpy grayscale',
-            #            cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY))
+                # Display the picture in grayscale
+                # cv2.imshow('OpenCV/Numpy grayscale',
+                #            cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY))
 
-            print("fps: {}".format(1 / (time.time() - last_time)))
+                print("fps: {}".format(1 / (time.time() - last_time)))
 
-            # Press "q" to quit
-            # if cv2.waitKey(25) & 0xFF == ord("q"):
-            #     cv2.destroyAllWindows()
-            #     break
+                # Press "q" to quit
+                # if cv2.waitKey(25) & 0xFF == ord("q"):
+                #     cv2.destroyAllWindows()
+                #     break
+                
 # Create an instance of the PyScope class
 scope = pyscope()
 scope.test()
