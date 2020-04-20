f0fab9105043aa5038c34398c8e3fb309955cf01
Trevor Dobbertin <trevordobbertin@gmail.com>
2020-04-19 19:49:52 -0500

    Pygame was possibly working with the X display. Use the fast ss method.

--- CSGOpygame/pyscope.py
+++ CSGOpygame/pyscope.py
@@ -1,6 +1,7 @@
 import os
 import pygame
 import time
+import mss
 import random
 import numpy as np
 from PIL import Image
@@ -77,8 +78,33 @@ class pyscope :
         # Update the display
         pygame.display.update()
 
+    def fast_method():
+    with mss.mss(display="0.0") as sct:
+        # Part of the screen to capture
+        monitor = {"top": 40, "left": 0, "width": 800, "height": 640}
+
+        while "Screen capturing":
+            last_time = time.time()
+
+            # Get raw pixels from the screen, save it to a Numpy array
+            img = np.array(sct.grab(monitor))
+
+            # Display the picture
+            # cv2.imshow("OpenCV/Numpy normal", img)
+
+            # Display the picture in grayscale
+            # cv2.imshow('OpenCV/Numpy grayscale',
+            #            cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY))
+
+            print("fps: {}".format(1 / (time.time() - last_time)))
+
+            # Press "q" to quit
+            # if cv2.waitKey(25) & 0xFF == ord("q"):
+            #     cv2.destroyAllWindows()
+            #     break
 # Create an instance of the PyScope class
 scope = pyscope()
 scope.test()
 scope.screenshot()
+scope.fast_method()
 # print(timeit.Timer(scope.screenshot).timeit(120))
