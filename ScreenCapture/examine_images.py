d33c6dbcd88cbd0fd63fbe168410c2360047eee0
Trevor Dobbertin <trevordobbertin@gmail.com>
2020-04-21 13:14:55 -0500

    Change the test data gather to fix broken colors.

--- ScreenCapture/examine_images.py
+++ ScreenCapture/examine_images.py
@@ -4,14 +4,13 @@ import time
 
 
 d = np.load(
-    'c:/Users/trevo/OneDrive/TrevorProgramming/Laptop/CSGOMLBot/ScreenCapture/test.npy')
+    'c:/Users/trevo/OneDrive/TrevorProgramming/Laptop/CSGOMLBot/test.npy')
+
 
 for image in d:
-<<<<<<< HEAD
     print(image)
     cv2.imshow("OpenCV/Numpy normal", image)
-    time.sleep(.5)
-=======
-    img = Image.fromarray(image, 'BGRA')
-    img.show()
->>>>>>> 02b85f2d49623df612116541201c618936171e2f
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
+    time.sleep(.1)
--- ScreenCapture/numpy_mss.py
+++ ScreenCapture/numpy_mss.py
@@ -44,26 +44,45 @@ def fast_method():
             # Get raw pixels from the screen, save it to a Numpy array
             img_array = np.array(sct.grab(monitor))
 
+            print(img_array.shape)
+
             print("fps: {}".format(1 / (time.time() - last_time)))
 
 
+def to_np(images):
+    return np.asarray(images, dtype=np.float32)
+
 def fast_method_with_array():
-    with mss.mss(display=":1") as sct:
+    array_of_images = []
+    with mss.mss() as sct:
         # Part of the screen to capture
         monitor = {"top": 0, "left": 0, "width": 640, "height": 480}
-        array_of_images = []
-        count = 0
 
-        while count < 10:
+        for _ in range(50):
             last_time = time.time()
 
             # Get raw pixels from the screen, save it to a Numpy array
-            img_array = np.array(sct.grab(monitor))
+            sct_img = sct.grab(monitor)
 
-            array_of_images.append(img_array)
+            # print(dir(sct_img))
+
+            array_of_images.append(np.array(sct_img))
 
             print("fps: {}".format(1 / (time.time() - last_time)))
-            count+=1
+
+    np.save('test', array_of_images)
+
+
+def show_images(array):
+     for image in array:
+        cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        cv2.imshow('Test', image)
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+        time.sleep(.1)
 
 
 if __name__ == '__main__':
